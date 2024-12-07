b4dd187821307aae054c011e906baed61d3c8ad6
Jan Badenhorst <jan@trintel.co.za>
2024-12-07 12:11:34 +0200

    Prep for day-8

---
+++ day_8/day_8.js
@@ -0,0 +1,11 @@
+import {linesToArray, logIt, readInput, } from "../utils.js";
+
+// Parse Input
+let task1 = 0;
+let task2 = 0;
+const lines = linesToArray(readInput('aoc_8_0.txt'));
+logIt(lines);
+
+
+logIt(task1);
+logIt(task2);
