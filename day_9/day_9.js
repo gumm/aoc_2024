5e8a656cf34bb6e6ca2596f360ec85ab5ba774df
Jan Badenhorst <jan@trintel.co.za>
2024-12-08 10:08:56 +0200

    Prep for Day-9

---
+++ day_9/day_9.js
@@ -0,0 +1,9 @@
+import {inspect, linesToArray, readInput,} from "../utils.js";
+
+let task1 = 0
+let task2 = 0
+const matrix = linesToArray(readInput('aoc_9_e.txt')).map(e => e.split(''));
+inspect(matrix);
+
+inspect(task1);
+inspect(task2);
