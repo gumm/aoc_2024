c94f2c098a758960127f3c7c97702f17e6ecac8d
Jan Badenhorst <jan@trintel.co.za>
2024-12-11 21:34:14 +0200

    Day 11

--- day_11/day_11.js
+++ day_11/day_11.js
@@ -1,13 +1,16 @@
-import {inspect, lineToIntArr, readFile, sumArr} from "../utils.js";
+import {lineToIntArr, readFile, sumArr} from "../utils.js";
 import * as B from "badu";
 
 const inputMap = lineToIntArr(readFile('aoc_11_0.txt'))
   .reduce((p, c) => p.set(c, (p.get(c) || 0) + 1), new Map());
 
+// Helpers
 const wrapSkip = e => ({n: e, d: false})
 const wrap = e => ({n: e, d: true})
 const unwrap = e => e.n;
 const isDone = e => e.d;
+
+// Rules
 const rule1 = e => e === 0 ? wrap(1) : wrapSkip(e);
 const rule2 = w => {
   if (isDone(w)) return w;
@@ -19,15 +22,21 @@ const rule2 = w => {
 const rule3 = w => isDone(w) ? (unwrap(w).length ? unwrap(w) : [unwrap(w)]) : [unwrap(w) * 2024]
 const blink = B.compose(rule3, rule2, rule1);
 
-let a = inputMap;
-for (let i = 0; i < 75; i++) {
-  a = [...a.entries()].reduce((p, [k, v]) => {
-    blink(k).forEach(e => {
-      const multiplier = p.get(e) || 0;
-      p.set(e, multiplier + v);
-    });
-    return p;
-  }, new Map());
+// Go for it.
+const task1 = 25;
+const task2 = 75;
+const compute = (a, iter) => {
+  for (let i = 0; i < iter; i++) {
+    a = [...a.entries()].reduce((p, [k, v]) => {
+      blink(k).forEach(e => p.set(e, (p.get(e) || 0) + v));
+      return p;
+    }, new Map());
+  }
+  return a;
 }
+const r1 = compute(inputMap, task1);
+const r2 = compute(r1, task2 - task1);
+console.log('Task1', sumArr(r1.values()));
+console.log('Task2', sumArr(r2.values()));
 
-inspect(sumArr(a.values()));
+
